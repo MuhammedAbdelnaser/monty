e4ff4cd656bb3d2def79cb1e47f2724df4c0cfee
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-05-26 23:35:57 +0300

    add node to the head stack

---
+++ add_queue.c
@@ -0,0 +1,36 @@
+#include "monty.h"
+
+/**
+ * add_queue - add node to the tail stack
+ * @n: new_value
+ * @head: head of the stack
+ * Return: no return
+ */
+void add_queue(my_stack_t **head, int n)
+{
+    my_stack_t *new_node = malloc(sizeof(my_stack_t));
+    if (new_node == NULL)
+    {
+        printf("Error\n");
+        return;
+    }
+
+    new_node->n = n;
+    new_node->next = NULL;
+
+    if (*head == NULL)
+    {
+        *head = new_node;
+        new_node->prev = NULL;
+        return;
+    }
+
+    my_stack_t *aux = *head;
+    while (aux->next != NULL)
+    {
+        aux = aux->next;
+    }
+
+    aux->next = new_node;
+    new_node->prev = aux;
+}
