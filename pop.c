e778cc8fbc7a56428a6e722cc8d973239e9c3efc
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-05-27 01:12:38 +0300

    fix pop

--- pop.c
+++ pop.c
@@ -1,23 +1,24 @@
 #include "monty.h"
 
 /**
- * s_pop - removes the top element of the stack
+ * s_pop - prints the top
  * @head: stack head
  * @counter: line_number
  * Return: no return
- */
+*/
 void s_pop(my_stack_t **head, unsigned int counter)
 {
-    if (*head == NULL)
-    {
-        fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
-        fclose(bus.file);
-        free(bus.content);
-        free_stack(*head);
-        exit(EXIT_FAILURE);
-    }
-    
-    my_stack_t *h = *head;
-    *head = h->next;
-    free(h);
+	my_stack_t *h;
+
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	*head = h->next;
+	free(h);
 }
