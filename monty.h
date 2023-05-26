74e2d74c53d70645ad850d2447e5176f98cfef44
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-05-27 01:19:01 +0300

    header file

--- monty.h
+++ monty.h
@@ -1,9 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define _GNU_SOURCE
+
 #include <stdio.h> /* File, fopen, getline, fclose, stderr */
 #include <stdlib.h> /* EXIT_SUCCESS, EXIT_FAILURE, free */
-
+#include <unistd.h> /* isatty, write, STDIN_FILENO */
+#include <ctype.h> /* isdigit */
+#include <sys/types.h> /* ssize_t */
 #include <string.h> /* strtok, strcmp */
 
 /**
@@ -20,7 +24,7 @@ typedef struct stack_s
 	int n;
 	struct stack_s *prev;
 	struct stack_s *next;
-} my_stack_t;
+} stack_t;
 
 /**
  * struct instruction_s - opcode and its function
@@ -33,7 +37,7 @@ typedef struct stack_s
 typedef struct instruction_s
 {
 	char *opcode;
-	void (*f)(my_stack_t **stack, unsigned int line_number);
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 
@@ -46,17 +50,20 @@ typedef struct bus_s
 }  bus_t;
 extern bus_t bus;
 
-int execute(char *content, my_stack_t **head, unsigned int counter,
+int execute(char *content, stack_t **head, unsigned int line_number,
 FILE *file);
-void s_push(my_stack_t **head, unsigned int number);
-void s_pall(my_stack_t **head, unsigned int number);
-void s_pint(my_stack_t **head, unsigned int number);
-void s_pop(my_stack_t **head, unsigned int number);
-void s_swap(my_stack_t **head, unsigned int number);
-void s_add(my_stack_t **head, unsigned int number);
-void s_nop(my_stack_t **head, unsigned int number);
-void free_stack(my_stack_t *head);
-void add_node(my_stack_t **head, int n);
-void add_queue(my_stack_t **head, int n);
+void push(stack_t **head, unsigned int line_number);
+void pall(stack_t **head, unsigned int line_number);
+void free_stack(stack_t *head);
+void add_node(stack_t **head, int n);
+void pint(stack_t **head, unsigned int line_number);
+void pop(stack_t **head, unsigned int line_number);
+void swap(stack_t **head, unsigned int line_number);
+void fadd(stack_t **head, unsigned int line_number);
+void nop(stack_t **head, unsigned int line_number);
+void sub(stack_t **head, unsigned int line_number);
+void fdiv(stack_t **head, unsigned int line_number);
+void mul(stack_t **head, unsigned int line_number);
+void mod(stack_t **head, unsigned int line_number);
 
 #endif
