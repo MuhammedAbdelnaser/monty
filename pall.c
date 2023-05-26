b052402925a0f6acb13b8ecb9e5cb08e68f8fe5a
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-05-27 01:00:41 +0300

    modify pall

--- pall.c
+++ pall.c
@@ -1,22 +1,21 @@
 #include "monty.h"
-
 /**
- * s_pall - prints the stack
+ * f_pall - prints the stack
  * @head: stack head
  * @counter: no used
  * Return: no return
 */
-void s_pall(my_stack_t **head, unsigned int counter)
+void f_pall(stack_t **head, unsigned int counter)
 {
-	my_stack_t *h;
+	stack_t *h;
 	(void)counter;
 
 	h = *head;
 	if (h == NULL)
 		return;
-	while (h)
-	{
+	do {
 		printf("%d\n", h->n);
 		h = h->next;
 	}
+	while (h)
 }
