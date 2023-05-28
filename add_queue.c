7e96308d21623613d5c8b43a8f50a3ad6e5fa244
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-05-28 22:34:36 +0300

    fix queue

--- add_queue.c
+++ add_queue.c
@@ -2,11 +2,11 @@
 
 /**
  * add_queue - add node to the tail stack
- * @n: new_value
+ * @new: new_value
  * @head: head of the stack
  * Return: no return
 */
-void add_queue(stack_t **head, int n)
+void add_queue(stack_t **head, int new)
 {
 	stack_t *new_node, *aux;
 
@@ -16,7 +16,7 @@ void add_queue(stack_t **head, int n)
 	{
 		printf("Error\n");
 	}
-	new_node->n = n;
+	new_node->n = new;
 	new_node->next = NULL;
 	if (aux)
 	{
