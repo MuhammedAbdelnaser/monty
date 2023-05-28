45f84e23fe2f7207fb4f7220e2dd0c55903406a2
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-05-28 22:34:29 +0300

    dix add node

--- add_node.c
+++ add_node.c
@@ -3,10 +3,10 @@
 /**
  * add_node - add node to the head stack
  * @head: head of the stack
- * @n: new_value
+ * @new: new_value
  * Return: no return
 */
-void add_node(stack_t **head, int n)
+void add_node(stack_t **head, int new)
 {
 
 	stack_t *new_node, *aux;
@@ -18,7 +18,7 @@ void add_node(stack_t **head, int n)
 		exit(0); }
 	if (aux)
 		aux->prev = new_node;
-	new_node->n = n;
+	new_node->n = new;
 	new_node->next = *head;
 	new_node->prev = NULL;
 	*head = new_node;
