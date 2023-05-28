e1e99958486ccc613e99b4d090cb86b16f543d3c
Muhammed Abdelnaser <67874300+MuhammedAbdelnaser@users.noreply.github.com>
2023-05-28 22:18:30 +0300

    frees a doubly linked list

--- free_stack.c
+++ free_stack.c
@@ -4,9 +4,9 @@
 * free_stack - frees a doubly linked list
 * @head: head of the stack
 */
-void free_stack(my_stack_t *head)
+void free_stack(stack_t *head)
 {
-	my_stack_t *aux;
+	stack_t *aux;
 
 	aux = head;
 	while (head)
